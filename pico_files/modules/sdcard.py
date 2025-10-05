d208ddba2f9e730b7bd2641aeb7f6d5da6619d7a
bjsoftab4 <kaervek@yahoo.co.jp>
2025-10-05 18:50:48 +0900

    pico_files/modules/sdcard.py: improve sd speed In read_into(), sleep_ms(1) is too long

--- pico_files/modules/sdcard.py
+++ pico_files/modules/sdcard.py
@@ -186,11 +186,11 @@ class SDCard:
         self.cs(0)
 
         # read until start byte (0xff)
-        for i in range(_CMD_TIMEOUT):
+        for i in range(_CMD_TIMEOUT * 50):
             self.spi.readinto(self.tokenbuf, 0xFF)
             if self.tokenbuf[0] == _TOKEN_DATA:
                 break
-            time.sleep_ms(1)
+            time.sleep_us(20)
         else:
             self.cs(1)
             raise OSError("timeout waiting for response")
